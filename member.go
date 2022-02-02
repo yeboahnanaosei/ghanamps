d2b6e2ab802a2625685e7ce73e99b2d7fab60fb0
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2022-02-02 11:43:08 +0000

    Add full party name to response

--- member.go
+++ member.go
@@ -10,8 +10,10 @@ import (
 
 // Member represents the details of one member of parliament
 type Member struct {
+	ID           string `json:"-"`
 	Name         string `json:"name"`
 	Party        string `json:"party"`
+	PartyAbbr    string `json:"partyAbbr"`
 	Constituency string `json:"constituency"`
 	Region       string `json:"region"`
 	Photo        string `json:"photo"`
@@ -23,29 +25,45 @@ const baseURL string = "https://www.parliament.gh"
 
 // Fetch fetches the current members of parliament
 func Fetch() ([]Member, error) {
-	members := []Member{}
-	c := colly.NewCollector()
-	pageVisitor := colly.NewCollector(colly.Async(true))
+	// mux := sync.Mutex{}
+	members := map[string]Member{}
+	payload := []Member{}
 
-	c.OnHTML("a.square", func(e *colly.HTMLElement) {
+	mainCollector := colly.NewCollector()
+	pageVisitor := colly.NewCollector(colly.Async(true))
+	profileVisitor := colly.NewCollector(colly.Async(true))
+
+	mainCollector.OnHTML("a.square", func(e *colly.HTMLElement) {
 		pageVisitor.Visit(baseURL + "/" + e.Attr("href"))
 	})
 
 	pageVisitor.OnHTML("div.mpcard", func(e *colly.HTMLElement) {
 		member := Member{}
+		member.Photo = strings.TrimSpace(baseURL + "/" + e.ChildAttr("a > img", "src"))
 		member.Name = strings.TrimSpace(strings.ToUpper(e.ChildText("b.padd")))
 		member.Constituency = strings.TrimSpace(strings.ToUpper(e.ChildText("a > div > center b:nth-of-type(2)")))
 		member.Region = strings.TrimSpace(strings.ToUpper(e.ChildText("a > div > center span:nth-of-type(1)")))
-		member.Photo = strings.TrimSpace(baseURL + "/" + e.ChildAttr("a > img", "src"))
-		member.Party = strings.TrimSpace(e.ChildText("a > div > center :nth-child(2)"))
+		member.PartyAbbr = strings.TrimSpace(e.ChildText("a > div > center :nth-child(2)"))
 
-		mpNumber := strings.TrimSpace(strings.TrimSuffix(path.Base(member.Photo), path.Ext(member.Photo)))
-		member.Profile = baseURL + fmt.Sprintf("/mps?mp=%s", mpNumber)
+		memberNumber := strings.TrimSpace(strings.TrimSuffix(path.Base(member.Photo), path.Ext(member.Photo)))
+		member.Profile = baseURL + fmt.Sprintf("/mps?mp=%s", memberNumber)
 
-		members = append(members, member)
+		members[memberNumber] = member
+		profileVisitor.Visit(member.Profile)
 	})
 
-	c.Visit(baseURL + "/mps?az")
+	profileVisitor.OnHTML("div.fl > table", func(e *colly.HTMLElement) {
+		partyParts := strings.Split(e.ChildText("tr:nth-child(2) > td:nth-child(2)"), "(")
+		memberNumber := e.Request.URL.Query().Get("mp")
+		member := members[memberNumber]
+		if len(partyParts) > 1 {
+			member.Party = strings.TrimSpace(strings.ToUpper(partyParts[0]))
+			payload = append(payload, member)
+		}
+	})
+
+	mainCollector.Visit(baseURL + "/mps?az")
 	pageVisitor.Wait()
-	return members, nil
+	profileVisitor.Wait()
+	return payload, nil
 }
