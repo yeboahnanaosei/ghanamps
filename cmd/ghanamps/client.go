0a51f2bd90c89a4ccaba21d9144049ee79f72609
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2022-02-04 15:23:07 +0000

    Handle empty response

--- cmd/ghanamps/client.go
+++ cmd/ghanamps/client.go
@@ -42,6 +42,11 @@ func handleGetMembers(party string) {
 		os.Exit(1)
 	}
 
+	if len(payload.Data.([]interface{})) == 0 {
+		fmt.Println("ghanamps: no members found for the given party: " + party)
+		os.Exit(0)
+	}
+
 	data, err := json.MarshalIndent(payload.Data, "", "  ")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ghanamps: %s\n", err)
