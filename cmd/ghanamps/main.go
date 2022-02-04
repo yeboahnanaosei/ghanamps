501b3f9bf11265c0391c581f063e1dd8d371c24b
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2022-02-04 07:44:09 +0000

    Add flags to fetch either members or leaders

--- cmd/ghanamps/main.go
+++ cmd/ghanamps/main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"os"
 
@@ -11,16 +12,44 @@ import (
 )
 
 func main() {
-	members, err := ghanamps.Fetch()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ghanamps: error fetching members: %s", err)
+	fetchMembers := flag.Bool("members", false, "returns the current members of Ghana's parliament")
+	fetchLeaders := flag.Bool("leaders", false, "returns the current leaders of Ghana's parliament")
+	flag.Parse()
+
+	if !*fetchMembers && !*fetchLeaders {
+		fmt.Fprint(os.Stderr, "ghanamps: you will have to specify one of -members or -leaders\n")
+		flag.Usage()
 		os.Exit(1)
+	}
+	if *fetchMembers && *fetchLeaders {
+		fmt.Fprint(os.Stdout, "ghanapms: please specify only one of the flags: members or leaders\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	var payload interface{}
+	var err error
+
+	if *fetchMembers {
+		payload, err = ghanamps.Members()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ghanamps: error fetching members: %s\n", err)
+			os.Exit(1)
+		}
+	}
+
+	if *fetchLeaders {
+		payload, err = ghanamps.Leaders()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ghanamps: error fetching leaders: %s\n", err)
+			os.Exit(1)
+		}
 	}
 
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetEscapeHTML(false)
 	enc.SetIndent(" ", "  ")
-	if err := enc.Encode(members); err != nil {
-		fmt.Fprintf(os.Stderr, "ghanamps: failed to fetch members: %s", err)
+	if err := enc.Encode(payload); err != nil {
+		fmt.Fprintf(os.Stderr, "ghanamps: failed to fetch leaders: %s", err)
 	}
 }
