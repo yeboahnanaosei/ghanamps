6b3874b4b172d576a85ad5434b13f7af3e5eed5f
Nana Osei Yeboah <yeboahnanaosei@gmail.com>
2022-02-04 07:02:39 +0000

    Add leadership endpoint

--- cmd/ghanampsd/main.go
+++ cmd/ghanampsd/main.go
@@ -12,9 +12,11 @@ import (
 	"github.com/yeboahnanaosei/ghanamps"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+// membersHandler handles the members endpoint. It returns a JSON of the current
+// members of parliament.
+func membersHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	members, err := ghanamps.Fetch()
+	members, err := ghanamps.Members()
 	if err != nil {
 		log.Println("error fetching members:", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -45,13 +47,49 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// leadershipHandler returns handles the leadership endpoint. It returns a JSON
+// of the current leaders of parliament.
+func leadershipHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	leaders, err := ghanamps.Leaders()
+	if err != nil {
+		log.Println("error fetching leadership:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"ok": false, "msg": "Request failed. An internal error occured"}`))
+		return
+	}
+
+	enc := json.NewEncoder(w)
+	enc.SetEscapeHTML(false)
+	enc.SetIndent(" ", "  ")
+
+	payload := struct {
+		Ok   bool              `json:"ok"`
+		Msg  string            `json:"msg"`
+		Data []ghanamps.Leader `json:"data"`
+	}{
+		Ok:   true,
+		Msg:  "Request successful",
+		Data: leaders,
+	}
+
+	err = enc.Encode(payload)
+	if err != nil {
+		log.Println("error encoding JSON response:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"ok": false, "msg": "Request failed. An internal error occured"}`))
+		return
+	}
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000"
 	}
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/members", membersHandler)
+	http.HandleFunc("/leaders", leadershipHandler)
 	log.Println("listening on port:", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
